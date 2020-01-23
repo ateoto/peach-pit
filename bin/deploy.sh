8c488ef6ff8cc192e5558c4c24368718dedf95ed
Matthew McCants <matt@mccants.io>
2020-01-22 22:23:38 -0500

    Slight update.

---
+++ bin/deploy.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# https://stackoverflow.com/a/246128
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+APP_ROOT="$(dirname "$(dirname "$(readlink -fm "$0")")")"
+
+ansible-playbook \
+  -i "${APP_ROOT}/deployment/ansible/hosts" \
+  "${APP_ROOT}/deployment/ansible/site.yml"
